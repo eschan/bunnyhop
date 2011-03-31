50b52ecfccce11f8bb4dd882f8ee4ae561b13811
Edward Chan <wado03@hotmail.com>
2011-03-30 17:04:09 -0700

    code cleanup

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var bunnyhop = require('./lib/bunnyhop')
-var fs = require('fs')
-
-var configFile = process.env.BUNNYHOP_CONFIG || "bunnyhop.cfg.example";
-var config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
 
 if (module == process.mainModule) {
-	var bunhop = bunnyhop(config);
+	var bunhop = bunnyhop();
 	bunhop.start();
   console.log("bunnyhop started!!!");
 }
