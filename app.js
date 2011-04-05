3646bf2b3359c3d3943415c232d44d9309254fc5
Edward Chan <wado03@hotmail.com>
2011-04-05 16:26:24 -0700

    added package.json

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var Bunnyhop = require('./lib/bunnyhop')
-var fs = require('fs');
 
 if (module == process.mainModule) {
-	var configFile = process.env.BUNNYHOP_CONFIG || "/etc/bunnyhop.cfg";
-	var config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
-
-	var bunhop = new Bunnyhop(config);
-	bunhop.listen();
+	var bunnyhop = new Bunnyhop();
+	bunnyhop.listen();
   console.log("bunnyhop started.");
 }
