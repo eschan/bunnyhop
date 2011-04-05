03350a9bcc7c3bb09e30cc6e483437064c608c14
Edward Chan <wado03@hotmail.com>
2011-04-05 15:32:40 -0700

    instance variables and retryOnPoison refactor... lots of refactoring

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var bunnyhop = require('./lib/bunnyhop')
+var Bunnyhop = require('./lib/bunnyhop')
+var fs = require('fs');
 
 if (module == process.mainModule) {
-	var bunhop = bunnyhop();
-	bunhop.start();
-  console.log("bunnyhop started!!!");
+	var configFile = process.env.BUNNYHOP_CONFIG || "/etc/bunnyhop.cfg";
+	var config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+
+	var bunhop = new Bunnyhop(config);
+	bunhop.listen();
+  console.log("bunnyhop started.");
 }
